14813eb3d54600f06400f0dcafc827ab8b344789
Melagrin <150378164+Melagrin@users.noreply.github.com>
2026-04-01 21:14:23 +0300

    test4

--- code/config/config.cjs
+++ code/config/config.cjs
@@ -14,11 +14,8 @@ module.exports = {
     host: process.env.DATABASE_HOST,
   },
   test: {
-    dialect: 'postgres',
-    database: process.env.DATABASE_NAME,
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    port: process.env.DATABASE_PORT,
-    host: process.env.DATABASE_HOST,
+    dialect: 'sqlite',
+    storage: ':memory:',
+    logging: false,
   },
 };
